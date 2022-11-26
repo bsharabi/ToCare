ab4162ee117ab413c21cf6e224697911d4389a53
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-11-26 15:45:59 +0200

    new app with login

---
+++ app/src/main/java/com/example/tocare/Activity_Splash_Login.java
@@ -0,0 +1,14 @@
+package com.example.tocare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Splash_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_login);
+    }
+}

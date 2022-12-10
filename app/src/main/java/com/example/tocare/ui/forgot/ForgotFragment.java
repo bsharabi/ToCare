16bbf80a4154febd9c1ca4025baae596682677da
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-12-10 15:37:07 +0200

    change from notificationsViewModel to forgotViewModel and add click listener for forgot button without implementation

--- app/src/main/java/com/example/tocare/ui/forgot/ForgotFragment.java
+++ app/src/main/java/com/example/tocare/ui/forgot/ForgotFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,36 +16,51 @@ import com.example.tocare.databinding.FragmentForgotBinding;
 import com.example.tocare.ui.login.LoginFragment;
 import com.example.tocare.ui.signup.SignupFragment;
 
-public class ForgotFragment extends Fragment {
+public class ForgotFragment extends Fragment implements View.OnClickListener {
 
     private FragmentForgotBinding binding;
     private static ForgotFragment single_instance = null;
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        ForgotViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(ForgotViewModel.class);
+    TextView inputEmail;
+    Button btSubmit;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ForgotViewModel forgotViewModel = new ViewModelProvider(this).get(ForgotViewModel.class);
 
         binding = FragmentForgotBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textForgot;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        forgotViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        inputEmail = binding.etEmail;
+        btSubmit=binding.btSubmit;
+        btSubmit.setOnClickListener(this);
+
         return root;
+    }
+
+    private void forgotPassword() {
+        System.out.println("Forgot password listener");
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+        System.out.println("onDestroyView Function");
     }
+
     private ForgotFragment() {
     }
 
     public static ForgotFragment getInstance() {
         if (single_instance == null)
             single_instance = new ForgotFragment();
-
         return single_instance;
     }
 
+    @Override
+    public void onClick(View view) {
+        forgotPassword();
+    }
 }

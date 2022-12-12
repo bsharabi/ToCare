03443e6e175da111cbaf9a9aaaf9d1bf0b9bb33e
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-12-12 18:19:00 +0200

    add listener to button back

--- app/src/main/java/com/example/tocare/UIL/ui/Activities/ManageUsersActivity.java
+++ app/src/main/java/com/example/tocare/UIL/ui/Activities/ManageUsersActivity.java
@@ -2,7 +2,11 @@ package com.example.tocare.UIL.ui.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.InflateException;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.tocare.R;
 import com.example.tocare.databinding.ActivityManageUsersBinding;
@@ -10,10 +14,25 @@ import com.example.tocare.databinding.ActivityManageUsersBinding;
 public class ManageUsersActivity extends AppCompatActivity {
 
     private ActivityManageUsersBinding binding;
+    private Button btBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_users);
+
+        binding = ActivityManageUsersBinding.inflate(getLayoutInflater());
+        btBack = binding.btBack;
+
+
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ManageUsersActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
 }

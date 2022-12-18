a935446f7bf68acf4b73720608fa0c4388e45465
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-12-18 14:15:12 +0200

    update full-name to name and last name and replace data structure of children from list to hashmap

--- app/src/main/java/com/example/tocare/BLL/Departments/Admin.java
+++ app/src/main/java/com/example/tocare/BLL/Departments/Admin.java
@@ -1,38 +1,39 @@
 package com.example.tocare.BLL.Departments;
+import java.util.ArrayList;
 import java.util.HashMap;
+
 import java.util.Map;
 
 public class Admin extends UserModel {
 
-    private static Admin single_instance = null;
-    private Map<String, User> children;
+
+    private Map<String,User> childrenId;
 
     //---------------------------- Constructor ----------------------------------
-    public Admin(String id, String userName, String fullName, String phone, String bio, String imageUrl, boolean isAdmin) {
-        super(id, userName, fullName, phone, bio, imageUrl, isAdmin);
-        this.children=new HashMap<>();
+    public Admin(String id, String userName, String name,String lastName, String phone, String bio, String imageUrl, boolean isAdmin) {
+        super(id, userName, name,lastName, phone, bio, imageUrl, isAdmin);
+        this.childrenId=new HashMap<>();
     }
 
-    public Admin(String id, String userName, String fullName, String phone, String bio, String imageUrl, boolean isAdmin, Map<String, User> children) {
-        super(id, userName, fullName, phone, bio, imageUrl, isAdmin);
-        this.children = children;
+    public Admin(String id, String userName, String name,String lastName, String phone, String bio, String imageUrl, boolean isAdmin, Map<String,User> children) {
+        super(id, userName, name,lastName, phone, bio, imageUrl, isAdmin);
+        this.childrenId = children;
     }
 
-    public Admin(String id, String userName, String fullName, String phone, String bio, String imageUrl, boolean isAdmin, Map<String, Object> tasks, Map<String, User> children) {
-        super(id, userName, fullName, phone, bio, imageUrl, isAdmin, tasks);
-        this.children = children;
+    public Admin(String id, String userName, String name,String lastName, String phone, String bio, String imageUrl, boolean isAdmin, ArrayList<String> tasks, Map<String,User> children) {
+        super(id, userName, name,lastName, phone, bio, imageUrl, isAdmin, tasks);
+        this.childrenId = children;
     }
     public Admin(){
-
+        childrenId=new HashMap<>();
     }
 
     //---------------------------- Getter&&Setter -------------------------------
-
-    public Map<String, User> getChildren() {
-        return children;
+    public Map<String,User> getChildrenId() {
+        return childrenId;
     }
 
-    public void setChildren(Map<String, User> children) {
-        this.children = children;
+    public void setChildrenId(Map<String,User> childrenId) {
+        this.childrenId = childrenId;
     }
 }

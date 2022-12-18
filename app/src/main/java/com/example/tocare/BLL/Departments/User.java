0f77d4d240e4d09e0c4a830737240e7421d3958e
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-12-18 14:19:50 +0200

    add toString, constructor and replace full name to name and last name

--- app/src/main/java/com/example/tocare/BLL/Departments/User.java
+++ app/src/main/java/com/example/tocare/BLL/Departments/User.java
@@ -3,9 +3,15 @@ package com.example.tocare.BLL.Departments;
 public class User extends UserModel {
 
 
-
-    public User(String id, String userName, String fullName, String phone, String bio, String imageUrl, boolean isAdmin) {
-        super(id, userName, fullName, phone, bio, imageUrl, isAdmin);
+    public User(String id, String userName, String name,String lastName, String phone, String bio, String imageUrl, boolean isAdmin) {
+        super(id, userName, name,lastName, phone, bio, imageUrl, isAdmin);
     }
 
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{}";
+    }
 }

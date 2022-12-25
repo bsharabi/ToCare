c095e444d22415fb0105c9301b4e1040f0cd4f5e
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-12-25 11:06:46 +0200

    new Serializable

--- app/src/main/java/com/example/tocare/BLL/Departments/Admin.java
+++ app/src/main/java/com/example/tocare/BLL/Departments/Admin.java
@@ -1,39 +1,48 @@
 package com.example.tocare.BLL.Departments;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import java.util.List;
 import java.util.Map;
 
-public class Admin extends UserModel {
+public class Admin extends UserModel implements Serializable {
 
 
-    private Map<String,User> childrenId;
+    private List<String> childrenId;
 
     //---------------------------- Constructor ----------------------------------
     public Admin(String id, String userName, String name,String lastName, String phone, String bio, String imageUrl, boolean isAdmin) {
         super(id, userName, name,lastName, phone, bio, imageUrl, isAdmin);
-        this.childrenId=new HashMap<>();
+        this.childrenId=new ArrayList<>();
     }
 
-    public Admin(String id, String userName, String name,String lastName, String phone, String bio, String imageUrl, boolean isAdmin, Map<String,User> children) {
+    public Admin(String id, String userName, String name,String lastName, String phone, String bio, String imageUrl, boolean isAdmin, List<String> children) {
         super(id, userName, name,lastName, phone, bio, imageUrl, isAdmin);
         this.childrenId = children;
     }
 
-    public Admin(String id, String userName, String name,String lastName, String phone, String bio, String imageUrl, boolean isAdmin, ArrayList<String> tasks, Map<String,User> children) {
+    public Admin(String id, String userName, String name,String lastName, String phone, String bio, String imageUrl, boolean isAdmin, Tasks tasks, List<String> children) {
         super(id, userName, name,lastName, phone, bio, imageUrl, isAdmin, tasks);
         this.childrenId = children;
     }
     public Admin(){
-        childrenId=new HashMap<>();
+        childrenId=new ArrayList<>();
     }
 
     //---------------------------- Getter&&Setter -------------------------------
-    public Map<String,User> getChildrenId() {
+    public List<String> getChildrenId() {
         return childrenId;
     }
 
-    public void setChildrenId(Map<String,User> childrenId) {
+    public void setChildrenId(List<String> childrenId) {
         this.childrenId = childrenId;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "childrenId=" + childrenId +
+                '}'+super.toString();
     }
 }

f28f7bf823bd6310bc641b122554d7d42df71375
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-12-25 11:05:45 +0200

    add constructor

--- app/src/main/java/com/example/tocare/BLL/Departments/User.java
+++ app/src/main/java/com/example/tocare/BLL/Departments/User.java
@@ -1,6 +1,8 @@
 package com.example.tocare.BLL.Departments;
 
-public class User extends UserModel {
+import java.io.Serializable;
+
+public class User extends UserModel implements Serializable {
 
 
     public User(String id, String userName, String name,String lastName, String phone, String bio, String imageUrl, boolean isAdmin) {
@@ -8,6 +10,7 @@ public class User extends UserModel {
     }
 
     public User() {
+        this.setImageUrl("https://firebasestorage.googleapis.com/v0/b/tocare-5b2eb.appspot.com/o/placeHolder.png?alt=media&token=fa1fa6f4-233e-4375-84cd-e7cdd6260c7a");
     }
 
     @Override

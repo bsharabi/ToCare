adaa2d90f020eee157061e0069fb159ce2ab8c32
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2022-12-25 11:08:00 +0200

    new SignupUserVerification class

---
+++ app/src/main/java/com/example/tocare/UIL/ui/phone/PhoneSignupVerificationFragment.java
@@ -0,0 +1,276 @@
+package com.example.tocare.UIL.ui.phone;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import com.example.tocare.BLL.Departments.User;
+import com.example.tocare.BLL.Validation.UserValidation;
+import com.example.tocare.ManageUsersActivity;
+import com.example.tocare.PhoneLoginActivity;
+import com.example.tocare.R;
+import com.example.tocare.UIL.ui.users.UsersFragment;
+import com.example.tocare.databinding.FragmentPhoneCodeVerificationBinding;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.concurrent.TimeUnit;
+
+public class PhoneSignupVerificationFragment extends Fragment implements View.OnClickListener, TextWatcher {
+
+    private FragmentPhoneCodeVerificationBinding binding;
+    private EditText inputPhoneCode;
+    private Button btSubmit;
+    private TextView header, sendAgain;
+    private ImageView imageView;
+    private ProgressDialog dialog;
+    private static final String TAG = "SignupVerification";
+    private String mVerificationId;
+    private ManageUsersActivity manageUsersActivity;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
+            Log.d(TAG, "onVerificationCompleted:" + credential);
+            verifyPhoneNumberWithCode(credential.getSmsCode());
+        }
+
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Log.w(TAG, "onVerificationFailed", e);
+            Toast.makeText(getContext(), "Verification Failed", Toast.LENGTH_SHORT).show();
+            if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                // Invalid request
+            } else if (e instanceof FirebaseTooManyRequestsException) {
+                // The SMS quota for the project has been exceeded
+            }
+            dialog.dismiss();
+        }
+
+        @Override
+        public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken token) {
+            super.onCodeSent(verificationId, token);
+
+            Log.d(TAG, "onCodeSent:" + verificationId);
+            Toast.makeText(getContext(), "Code sent ", Toast.LENGTH_SHORT).show();
+            dialog.dismiss();
+
+            mVerificationId = verificationId;
+            mResendToken = token;
+        }
+    };
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        PhoneCodeViewModel phoneViewModel = new ViewModelProvider(this).get(PhoneCodeViewModel.class);
+
+        binding = FragmentPhoneCodeVerificationBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        inputPhoneCode = binding.etPhoneCode;
+        btSubmit = binding.btSubmit;
+        header = binding.textPhoneCode;
+        sendAgain = binding.tvSendAgain;
+        imageView = binding.ivPhoneIcon;
+        mVerificationId = requireArguments().getString("verificationId");
+        manageUsersActivity = (ManageUsersActivity) getActivity();
+
+        dialog = new ProgressDialog(root.getContext());
+
+        phoneViewModel.getText().observe(getViewLifecycleOwner(), header::setText);
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        inputPhoneCode.setTranslationY(300);
+        btSubmit.setTranslationY(300);
+        header.setTranslationY(300);
+        sendAgain.setTranslationY(300);
+        imageView.setTranslationX(300);
+
+        final float alpha = 0;
+
+        inputPhoneCode.setAlpha(alpha);
+        btSubmit.setAlpha(alpha);
+        header.setAlpha(alpha);
+        sendAgain.setAlpha(alpha);
+        imageView.setAlpha(alpha);
+
+        imageView.animate().translationX(0).alpha(1).setDuration(1000).setStartDelay(350).start();
+        header.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(450).start();
+        inputPhoneCode.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(550).start();
+        sendAgain.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(650).start();
+        btSubmit.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(750).start();
+
+        btSubmit.setOnClickListener(this);
+        sendAgain.setOnClickListener(this);
+        btSubmit.setEnabled(false);
+        inputPhoneCode.addTextChangedListener(this);
+
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        if (UserValidation.isValidCode(inputPhoneCode.getText().toString().trim()))
+            btSubmit.setEnabled(true);
+        else
+            btSubmit.setEnabled(false);
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+    }
+
+    @Override
+    public void onClick(@NonNull View view) {
+        switch (view.getId()) {
+            case R.id.bt_submit:
+                String Code = inputPhoneCode.getText().toString().trim();
+                verifyPhoneNumberWithCode(Code);
+                break;
+            case R.id.tv_send_again:
+                String phone = "+" + requireArguments().getString("countryCode") + requireArguments().getString("phone").substring(1);
+                resendVerificationCode(phone, mResendToken);
+                Toast.makeText(getContext(), "re send Verification Code", Toast.LENGTH_LONG).show();
+            default:
+                break;
+        }
+    }
+
+
+    private void createSignInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        dialog.dismiss();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "signInWithCredential:success");
+                        CollectionReference collectionReference = FirebaseFirestore.getInstance().collection("User");
+                        DocumentReference documentReference = collectionReference.document(task.getResult().getUser().getUid());
+                        User userModel = new User(
+                                task.getResult().getUser().getUid(),
+                                getArguments().getString("userName"),
+                                getArguments().getString("name"),
+                                getArguments().getString("lastName"),
+                                getArguments().getString("phone"),
+                                "Welcome ToCare " + getArguments().get("userName"),
+                                "https://firebasestorage.googleapis.com/v0/b/tocare-5b2eb.appspot.com/o/placeHolder.png?alt=media&token=fa1fa6f4-233e-4375-84cd-e7cdd6260c7a",
+                                false);
+
+                        documentReference.set(userModel)
+                                .addOnCompleteListener(task1 -> {
+                                    if (task1.isSuccessful()) {
+                                        dialog.dismiss();
+                                        manageUsersActivity.getCurrentAdmin().getChildrenId().add(task.getResult().getUser().getUid());
+
+                                        FirebaseAuth.getInstance().updateCurrentUser(currentUser);
+                                        FirebaseFirestore.getInstance().collection("User").document(currentUser.getUid())
+                                                        .set(manageUsersActivity.getCurrentAdmin())
+                                                .addOnCompleteListener(task2 -> {
+                                                    if (task2.isSuccessful()) {
+                                                        Log.d(TAG, "DocumentReference:success");
+                                                        ((ManageUsersActivity) getActivity()).swapFragmentByFragmentClass(UsersFragment.class, null);
+//                                        Toast.makeText(getContext(), "The details have been successfully registered", Toast.LENGTH_SHORT).show();
+                                                    }
+                                                }).addOnFailureListener(e -> {
+                                                    dialog.dismiss();
+                                                    Log.d(TAG, "DocumentReference:failed");
+                                                    Toast.makeText(getContext(), "The details were not successfully registered " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                });
+
+
+                                        Log.d(TAG, "DocumentReference:success");
+//                                        Toast.makeText(getContext(), "The details have been successfully registered", Toast.LENGTH_SHORT).show();
+                                    }
+                                }).addOnFailureListener(e -> {
+                                    dialog.dismiss();
+                                    Log.d(TAG, "DocumentReference:failed");
+                                    Toast.makeText(getContext(), "The details were not successfully registered " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                });
+
+
+
+                    }
+                }).addOnFailureListener(failure -> {
+                    Log.w(TAG, "signInWithCredential:failure", failure);
+                    if (failure instanceof FirebaseAuthInvalidCredentialsException) {
+                        System.out.println(failure);
+
+                    }
+
+                });
+    }
+
+    private void resendVerificationCode(String phoneNumber, PhoneAuthProvider.ForceResendingToken token) {
+        dialog.setMessage("Please wait...");
+        dialog.setTitle("Resend Verification Code");
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+
+
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(FirebaseAuth.getInstance())
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(getActivity())                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .setForceResendingToken(token)     // ForceResendingToken from callbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    public void verifyPhoneNumberWithCode(String code) {
+        dialog.setMessage("Please wait...");
+        dialog.setTitle("Verify Phone Number With Code");
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.show();
+
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, code);
+
+        createSignInWithPhoneAuthCredential(credential);
+
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}

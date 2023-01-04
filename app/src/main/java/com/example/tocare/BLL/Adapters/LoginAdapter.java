5cbe2c5589afd503874fc44231a430c9f0a496b9
Barak sharbi <41991103+bsharabi@users.noreply.github.com>
2023-01-04 16:30:31 +0200

    change path

--- app/src/main/java/com/example/tocare/BLL/Adapters/LoginAdapter.java
+++ app/src/main/java/com/example/tocare/BLL/Adapters/LoginAdapter.java
@@ -4,8 +4,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
-import com.example.tocare.UIL.ForgotFragment;
-import com.example.tocare.UIL.LoginFragment;
+import com.example.tocare.UIL.Fragment.ForgotFragment;
+import com.example.tocare.UIL.Fragment.LoginFragment;
 import com.example.tocare.UIL.signup.SignupFragment;
 
 
